48841df63aa5ad572ef67806f33310fc02d6277f
avaschmidt <ava.schmidt25@d155.org>
2025-05-01 07:37:22 -0500

    more profile stuff

---
+++ CLCGrindsetApp/Views/TeacherProfileViewController.swift
@@ -0,0 +1,16 @@
+//  Created by AVA SCHMIDT on 5/1/25.
+
+
+import UIKit
+
+class TeacherProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+
+}
