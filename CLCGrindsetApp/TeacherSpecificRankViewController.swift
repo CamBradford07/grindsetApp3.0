7d30d672d8355002982bf0a341eee830d51df7e4
SwimEvan <evan.meyer26@d155.org>
2025-05-01 19:51:10 -0500

    purple ranking

---
+++ CLCGrindsetApp/TeacherSpecificRankViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TeacherSpecificRankViewController.swift
+//  CLCGrindsetApp
+//
+//  Created by EVAN MEYER on 5/1/25.
+//
+
+import UIKit
+
+class TeacherSpecificRankViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
