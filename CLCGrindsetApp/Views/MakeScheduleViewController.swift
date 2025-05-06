25a009e024f82b0bdae26c5d5740dfedc5fa5b82
esnoftz <esnoftz@gmail.com>
2025-05-06 08:08:32 -0500

    index stuff for grad reqs

--- CLCGrindsetApp/Views/MakeScheduleViewController.swift
+++ CLCGrindsetApp/Views/MakeScheduleViewController.swift
@@ -54,8 +54,16 @@ class MakeScheduleViewController: UIViewController, UITableViewDelegate, UITable
         let complete = UITableViewRowAction(style: .normal, title: "Complete") { (action, indexPath) in
             var swipedCourse = allCourses.first(where: {$0.courseName.contains(classClicked.selectedClasses[indexPath.row].courseName)})! //chatgpt help
             
+            
+            // FIX THIS STUFF -Eva
             var subjectIndex = GradArrays.classes.firstIndex(of: swipedCourse.subject)
-            print(subjectIndex)
+            print(swipedCourse.subject)
+            print("subject index \(subjectIndex)")
+            
+            
+            
+            
+            
             
             completedClasses.completedClasses.append(swipedCourse)
             classClicked.selectedClasses.remove(at: indexPath.row)
