187ba5bd6f71219412863fec8a599d83fbc29289
SwimEvan <evan.meyer26@d155.org>
2025-05-07 17:21:41 -0500

    moved logout

--- CLCGrindsetApp/Views/StudentMenuViewController.swift
+++ CLCGrindsetApp/Views/StudentMenuViewController.swift
@@ -16,7 +16,14 @@ class StudentMenuViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func logoutButton(_ sender: UIButton) {
+        AppData.currentStudent = Student(dict: ["": ""])
+        AppData.saveUserAndPass()
+        if let viewControllers = self.navigationController?.viewControllers, viewControllers.count >= 3 {
+            self.navigationController?.popToViewController(viewControllers[viewControllers.count - 3], animated: true)
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
